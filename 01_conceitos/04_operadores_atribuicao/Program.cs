224d4d1aeccb2df4ee6866d5e1ad654e739f51cf
Gustavo_S <sgustavo.vinicius.sdg@gmail.com>
2026-05-14 12:31:18 -0300

    Continuando estudos impacta aprendendo sobre operadores de atribuião

--- 01_conceitos/04_operadores_atribuicao/Program.cs
+++ 01_conceitos/04_operadores_atribuicao/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int x = 5;
+
+        //adicionar 5 ao valor atual de x
+
+        x = x + 5;
+        Console.WriteLine("Valor atual de x: " + x);
+        Console.ReadKey();
+
+        Console.WriteLine("-------------------");
+
+        //Para atribuir um valor a variavel.
+
+        x += 5;
+        Console.WriteLine("Valor atual de x: " + x);
+        Console.ReadKey();
+
+        Console.WriteLine("-------------------");
+
+        //Podemos Utilizar outros Operadores.
+
+        x -= 1;
+        Console.WriteLine("Valor atual de x após o x-=1: " + x);
+        Console.ReadKey();
+
+        Console.WriteLine("-------------------");
+
+        
+
     }
 }
+ 
